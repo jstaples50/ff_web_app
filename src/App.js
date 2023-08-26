083efc0f2cfd188d08cb88428be29aaf21a4f03f
Jared Staples <jstaplesmusic@gmail.com>
2023-08-26 18:27:59 -0500

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./Routes/Login";
 import Home from "./Routes/Home";
 import Announcement from "./Routes/Announcement";
@@ -8,10 +11,14 @@ function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
-      <Login />
-      <Home />
-      <Announcement />
-      <Profile />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/announcement" element={<Announcement />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
