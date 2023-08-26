d1482e3be43fea908431917825edd05777e5eced
Jared Staples <jstaplesmusic@gmail.com>
2023-08-26 18:38:09 -0500

    Created links in NavBar

--- src/Routes/NavBar.jsx
+++ src/Routes/NavBar.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
       <h2>NavBar Component</h2>
+      <ul style={{ listStyle: "none" }}>
+        <Link to={"/"}>
+          <li key={"home"}>Home</li>
+        </Link>
+        <Link to={"/login"}>
+          <li key={"login"}>Login</li>
+        </Link>
+        <Link to={"/announcement"}>
+          <li key={"announcement"}>Announcement</li>
+        </Link>
+        <Link to={"/profile"}>
+          <li key={"profile"}>Profile</li>
+        </Link>
+      </ul>
     </div>
   );
 };
