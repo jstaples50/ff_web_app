0456ce54c0abc61c744aa98edd8a011d84696e95
Jared Staples <jstaplesmusic@gmail.com>
2023-08-26 18:17:06 -0500

    loginEntry: created basic functionality for login form

--- src/Routes/login-components/LoginEntry.jsx
+++ src/Routes/login-components/LoginEntry.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginEntry = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div>
       <h2>LoginEntry Component</h2>
+      <h3>Login</h3>
+      <form>
+        <input
+          type="text"
+          value={username}
+          placeholder="username"
+          onChange={handleUsernameChange}
+        />
+        <p>Username Value: {username}</p>
+        <input
+          type="text"
+          value={password}
+          placeholder="password"
+          onChange={handlePasswordChange}
+        />
+        <p>Password Value: {password}</p>
+        <button disabled={username === "" || password === ""}>Submit</button>
+      </form>
     </div>
   );
 };
